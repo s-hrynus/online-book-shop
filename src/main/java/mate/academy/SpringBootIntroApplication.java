71b1c3447f7764c2f387607c753ac7a72352a855
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-09-13 16:31:44 +0300

    added Book entity, implemented BookRepository methods configured spring-boot

--- src/main/java/mate/academy/SpringBootIntroApplication.java
+++ src/main/java/mate/academy/SpringBootIntroApplication.java
@@ -1,13 +1,34 @@
 package mate.academy;
 
+import java.math.BigDecimal;
+import mate.academy.model.Book;
+import mate.academy.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootIntroApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootIntroApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book angelAndDevil = new Book();
+            angelAndDevil.setAuthor("Dan Brown");
+            angelAndDevil.setPrice(BigDecimal.valueOf(150));
+            angelAndDevil.setIsbn("978-33-0");
+            angelAndDevil.setTittle("Angel and Devil");
+            angelAndDevil.setDescription("awesome plot");
+            bookService.save(angelAndDevil);
+            System.out.println(bookService.findAll());
+        };
+    }
 }
---
+++ src/main/java/mate/academy/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,56 @@
+package mate.academy.repository.impl;
+
+import java.util.List;
+import mate.academy.model.Book;
+import mate.academy.repository.BookRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert book " + book
+                    + " into DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> getAllProductsQuery = session
+                    .createQuery("FROM Book", Book.class);
+            return getAllProductsQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get list of books ", e);
+
+        }
+    }
+}
