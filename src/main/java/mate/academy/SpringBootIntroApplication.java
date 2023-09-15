efa049c4e378afe147f8ea3386b50e782e55197d
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-09-15 17:38:17 +0300

    realized Controller and Mapper, added necessary dependencies into pom.xml, tested POST, GET methods in postman

--- src/main/java/mate/academy/SpringBootIntroApplication.java
+++ src/main/java/mate/academy/SpringBootIntroApplication.java
@@ -1,13 +1,9 @@
 package mate.academy;
 
-import java.math.BigDecimal;
-import mate.academy.model.Book;
 import mate.academy.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootIntroApplication {
@@ -16,19 +12,5 @@ public class SpringBootIntroApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootIntroApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book angelAndDevil = new Book();
-            angelAndDevil.setAuthor("Dan Brown");
-            angelAndDevil.setPrice(BigDecimal.valueOf(150));
-            angelAndDevil.setIsbn("978-33-0");
-            angelAndDevil.setTittle("Angel and Devil");
-            angelAndDevil.setDescription("awesome plot");
-            bookService.save(angelAndDevil);
-            System.out.println(bookService.findAll());
-        };
     }
 }
--- src/main/java/mate/academy/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/service/impl/BookServiceImpl.java
@@ -1,24 +1,40 @@
 package mate.academy.service.impl;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import mate.academy.dto.BookDto;
+import mate.academy.dto.CreateBookRequestDto;
+import mate.academy.mapper.BookMapper;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
 import mate.academy.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toDto(savedBook);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Can't find book with id " + id));
+        return bookMapper.toDto(book);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
