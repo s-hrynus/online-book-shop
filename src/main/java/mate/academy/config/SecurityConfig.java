63bd267984149d5a35fb40af14f8da5aeb10c6e1
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-11-06 21:22:58 +0200

    added readme.md

--- src/main/java/mate/academy/config/SecurityConfig.java
+++ src/main/java/mate/academy/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/auth/**")
+                                .requestMatchers("/auth/**", "/swagger-ui/**","/error")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
